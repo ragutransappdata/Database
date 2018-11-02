c66c1f9452d02d79f38f9b927d33df7f2762a6b7
ragutransappdata <44695632+ragutransappdata@users.noreply.github.com>
2018-11-02 17:28:38 +0530

    deploy

--- deploy2.sh
+++
@@ -1,8 +0,0 @@
-. /u03/env/ORCL.env
-which sqlplus
-sqlplus system/passwd  <<EOF
-select sysdate+1 from dual;
-SELECT username FROM dba_users WHERE username='SYSTEM';
-DROP table SYSTEM.name;
-EOF
-exit
